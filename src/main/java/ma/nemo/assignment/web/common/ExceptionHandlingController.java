9be2c8715ea13d2025da45b225bc8e4610018d21
ouadia.g <ouadia.gamrane@octo.ma>
2023-09-22 15:40:26 +0100

    small fix

--- src/main/java/ma/nemo/assignment/web/common/ExceptionHandlingController.java
+++ src/main/java/ma/nemo/assignment/web/common/ExceptionHandlingController.java
@@ -13,11 +13,11 @@ public class ExceptionHandlingController {
 
     @ExceptionHandler(ProductNotFound.class)
     public ResponseEntity<String> handleSoldeDisponibleInsuffisantException(Exception ex, WebRequest request) {
-        return new ResponseEntity<>("Pas de solde pas de transfer", null, HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS);
+        return new ResponseEntity<>("Produit non existant", null, HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS);
     }
 
     @ExceptionHandler(ProductAlreadyExists.class)
     public ResponseEntity<String> handleCompteNonExistantException(Exception ex, WebRequest request) {
-        return new ResponseEntity<>("Compte introuvable", null, HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>("Produit existe déjà", null, HttpStatus.UNAUTHORIZED);
     }
 }
